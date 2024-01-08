019f3626cefd5ff0be2b96c98600ba135df155c0
VenusCh001 <venus222btcse22@igdtuw.ac.in>
2024-01-08 20:42:38 +0530

    sorting problems done

---
+++ sorting/sort_fruit_names.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void sort(string *arr,int n){
+    for(int i=0;i<n-1;i++){
+        int min_index=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j] < arr[min_index]){
+                min_index=j;
+            }
+        }
+        if(arr[i]!=arr[min_index]){
+            swap(arr[i],arr[min_index]);
+        }
+    }
+}
+int main(){
+    string arr[]={"papaya","mango","grapes","watermelon"};
+    sort(arr,4);
+    for(int i=0;i<4;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
---
+++ sorting/sort_zero_and_nonZero.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int* sort(int *arr,int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]==0 and arr[j+1]>0){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    return arr;
+}
+int main(){
+    int arr[]={0,5,0,3,4,2};
+    sort(arr,6);
+    for(int i=0;i<6;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
