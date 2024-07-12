fa4ec5a306dfef11b9dbaf43d7aca394b4ec08ed
VenusCh001 <venus222btcse22@igdtuw.ac.in>
2024-07-13 00:20:06 +0530

    critical node and two sum added

--- doublelinkedlist/basicimplementation.cpp
+++ doublelinkedlist/basicimplementation.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 class node{
     public:
@@ -37,13 +38,18 @@ class doubleLL{
         }
         tail->next=newnode;
         newnode->prev=tail;
+        tail=newnode;
     }
     void insertAtkthposition(int k,int val){
-        int count=0;
+        int count=1;
         node*temp=head;
         while(count<k-1){
             temp=temp->next;
             count++;
+        }
+        if(temp==tail){
+            insertAttail(val);
+            return;
         }
         node*newnode=new node(val);
         temp->next->prev=newnode;
@@ -92,26 +98,108 @@ class doubleLL{
         node*temp=head;
         head=tail;
         tail=temp;
-        display();
+    }
+    bool palindrome(){
+        node*temp=head;
+        while(temp->next){
+            temp=temp->next;
+        }
+        node*temp2=temp;
+        temp=head;
+        while(temp2->next!=temp and temp2!=temp){
+            if(temp->data!=temp2->data){
+                return 0;
+            }
+            temp=temp->next;
+            temp2=temp2->prev;
+        }
+        return 1;
+    }
+    deleteNodeWithSameNeighbour(){
+        node*temp=head;
+        while(temp->next){
+            temp=temp->next;
+        }
+        node*current=temp->prev;
+        while(current->prev){
+            if(current->next->data==current->prev->data){
+                current->next->prev=current->prev;
+                current->prev->next=current->next;
+                node*todelete=current;
+                current=current->prev;
+                delete todelete;
+                continue;
+            }
+            current=current->prev;
+        }
+    }
+    vector<int> findDistance(){
+        int countNode=1;
+        vector<int> result(2,-1);
+        int prevNodeCount,firstNodeCount=-1,LastNodeCount,lastCriticalNode;
+        int countCriticalNode=0;
+        int minDistance=INT8_MAX;
+        node*temp=head;
+        if(head==NULL or head->next==NULL)return result;
+        temp=temp->next;
+        while(temp->next){
+            countNode++;
+            if(temp->data < temp->prev->data and temp->data < temp->next->data){
+                countCriticalNode++;
+                if(firstNodeCount==-1){
+                    firstNodeCount=countNode;
+                    prevNodeCount=countNode;
+                    continue;
+                }
+                lastCriticalNode=countNode;
+                minDistance=min(countNode-prevNodeCount,minDistance);
+                prevNodeCount=countNode;
+            }
+            else if(temp->data>temp->prev->data and temp->data>temp->next->data){
+                countCriticalNode++;
+                if(firstNodeCount==-1){
+                    firstNodeCount=countNode;
+                    prevNodeCount=countNode;
+                    continue;
+                }
+                lastCriticalNode=countNode;
+                minDistance=min(countNode-prevNodeCount,minDistance);
+                prevNodeCount=countNode;
+
+            }temp=temp->next;
+        }
+        int maxDistance=lastCriticalNode-firstNodeCount;
+        result[0]=minDistance;
+        result[1]=maxDistance;
+        return result;    
+
+    }
+   vector<int> findSum(int target){
+        node*begin=head;
+        node*end=tail;
+        int sum;
+        while(begin->prev!=end){
+            sum=begin->data+end->data;
+            if(sum==target){
+                return {begin->data,end->data};
+            }
+            else if(sum>target)end=end->prev;
+            else begin=begin->next;
+        }
+        return {-1,-1};
     }
 };
 int main(){
     doubleLL LL;
-    LL.insertAthead(1);
+    LL.insertAttail(1);
+    LL.insertAttail(3);
+    LL.insertAttail(6);
+    LL.insertAttail(6);
+    LL.insertAttail(7);
+    LL.insertAttail(19);
     LL.display();
-    LL.insertAthead(2);
-    LL.display();
-    LL.insertAthead(3);
-    LL.display();
-    LL.insertAtkthposition(2,39);
-    LL.insertAtkthposition(3,42);
-    LL.insertAtkthposition(4,49);
-
-    LL.display();
-    LL.deletenode(2);
-    LL.display();
-    cout<<"after reverse i get : "<<endl;
-    LL.reverse();
-    LL.display();
+    
+    vector<int>result=LL.findSum(10);
+    cout<<result[0]<<" "<<result[1]<<" "<<endl;
     return 0;
 }
