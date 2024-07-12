acec98cca903714ab573d502e2c99c7cf9ee3673
VenusCh001 <venus222btcse22@igdtuw.ac.in>
2024-07-12 22:15:13 +0530

    delete,reverse added

--- doublelinkedlist/basicimplementation.cpp
+++ doublelinkedlist/basicimplementation.cpp
@@ -38,13 +38,61 @@ class doubleLL{
         tail->next=newnode;
         newnode->prev=tail;
     }
-    
+    void insertAtkthposition(int k,int val){
+        int count=0;
+        node*temp=head;
+        while(count<k-1){
+            temp=temp->next;
+            count++;
+        }
+        node*newnode=new node(val);
+        temp->next->prev=newnode;
+        newnode->next=temp->next;
+        temp->next=newnode;
+        newnode->prev=temp;
+    }
+    void deletenode(int k){
+        if(head==NULL){
+            return;
+        }
+        else if(k==1){
+            node*todelete=head;
+            head=head->next;
+            if(head) head->prev=NULL;
+            else head=tail=NULL;
+            delete todelete;
+        }
+        else{
+            node*temp=head;
+            int count=1;
+            while(count<k){
+                temp=temp->next;
+                count++;
+            }
+            temp->next->prev=temp->prev;
+            temp->prev->next=temp->next;
+            delete temp;
+        }
+    }
     void display(){
         node*temp=head;
         while(temp){
             cout<<temp->data<<" ";
             temp=temp->next;
+        }cout<<endl;
+    }
+    void reverse(){
+        node*current=head;
+        while(current){
+            node*nextptr=current->next;
+            current->next=current->prev;
+            current->prev=nextptr;
+            current=nextptr;
         }
+        node*temp=head;
+        head=tail;
+        tail=temp;
+        display();
     }
 };
 int main(){
@@ -54,6 +102,16 @@ int main(){
     LL.insertAthead(2);
     LL.display();
     LL.insertAthead(3);
+    LL.display();
+    LL.insertAtkthposition(2,39);
+    LL.insertAtkthposition(3,42);
+    LL.insertAtkthposition(4,49);
+
+    LL.display();
+    LL.deletenode(2);
+    LL.display();
+    cout<<"after reverse i get : "<<endl;
+    LL.reverse();
     LL.display();
     return 0;
 }
